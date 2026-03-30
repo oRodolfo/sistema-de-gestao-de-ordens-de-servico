31c8ac7301864c6c7a9b3c9ef1f99509c0c03c8c
Enzo Zaia <zaiaenzo77seven@gmail.com>
2026-03-30 19:53:07 -0300

    style: arrumei o aviso de erro na tela de login

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="min-h-screen h-screen flex">
+    <div class="min-h-screen h-screen flex relative">
         <div class="w-3/8 bg-blue-800 flex flex-col items-center justify-center">
             <img src="@/assets/img/fho.png" alt="Logo da FHO" class="w-24 h-24 object-contain" />
 
@@ -23,7 +23,7 @@
                             v-model="email"
                             type="email"
                             placeholder="seu.email@fho.edu.br"
-                            class="border border-gray-300 rounded-lg px-4 py-3 text-sm outline-none focus:border-blue-600"
+                            :class="['border rounded-lg px-4 py-3 text-sm outline-none w-full', erro ? 'border-red-500 focus:border-red-500' : 'border-gray-300', 'focus:border-blue-600']"
                         />
                     </div>
 
@@ -34,7 +34,7 @@
                             v-model="senha"
                             type="password"
                             placeholder="********"
-                            class="border border-gray-300 rounded-lg px-4 py-3 text-sm outline-none focus:border-blue-600"
+                            :class="['border rounded-lg px-4 py-3 text-sm outline-none w-full', erro ? 'border-red-500 focus:border-red-500' : 'border-gray-300', 'focus:border-blue-600']"
                         />
                      </div>
 
@@ -42,7 +42,7 @@
                       <button 
                         type="submit"
                         :disabled="carregando"
-                        class="bg-blue-800 text-white py-3 rounded-lg font-semibold mt-2 hover:bg-blue-600 disabled:opacity-50 disabled:cursor-not-allowed"
+                        class="bg-blue-800 text-white py-3 rounded-lg font-semibold mt-2 hover:bg-blue-600 disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
                       >
                         {{carregando ? 'Entrando...' : 'Entrar'}}
                       </button>
@@ -53,6 +53,17 @@
 
             </div>
             
+        </div>
+    </div>
+
+    <!-- TOAST -->
+    <div
+        v-if="erro"
+        class="absolute top-4 left-0 right-0 flex justify-center z-50"
+    >
+        <div class="bg-red-500 text-white px-5 py-4 rounded-lg flex items-center gap-3">
+            <span>X</span>
+            <p class="text-sm font-medium">{{ erro }}</p>
         </div>
     </div>
 
@@ -77,7 +88,12 @@
 
         setTimeout(() => {
             carregando.value = false
-            router.push('/dashboard')
+            erro.value = 'Email ou senha inválidos'
+
+            setTimeout(() => {
+                erro.value = ''
+            }, 3000)
+            
         }, 2000);
         console.log(email.value, senha.value)
         //aqui vai a chamada pra API de login quando estiver pronta
