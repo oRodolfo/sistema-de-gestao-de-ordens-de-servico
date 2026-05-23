ebdd16f8de1f6e2571a7451764385f2c53acc806
Enzo Zaia <zaiaenzo77seven@gmail.com>
2026-05-23 12:36:11 -0300

    consertado o login que tinha quebrado, e nova pagina de filtros na lista de funcionarios implementado

--- backend/grupo_usuario/models.py
+++ backend/grupo_usuario/models.py
@@ -1,16 +1,24 @@
 from django.db import models
 from usuario.models import Usuario
 from grupo.models import Grupo
-# Create your models here.
 
-# Modelo para a tabela "grupo_usuario", que representa a relação entre usuários e grupos
 class GrupoUsuario(models.Model):
-
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, db_column='id_usuario')
-
-    grupo = models.ForeignKey(Grupo, on_delete=models.CASCADE, db_column='id_grupo')
+    usuario = models.ForeignKey(
+        Usuario,
+        on_delete=models.CASCADE,
+        db_column='id_usuario',
+        primary_key=True  # ← usa id_usuario como PK
+    )
+    grupo = models.ForeignKey(
+        Grupo,
+        on_delete=models.CASCADE,
+        db_column='id_grupo'
+    )
 
     class Meta:
         managed = False
         db_table = 'grupo_usuario'
         unique_together = (('usuario', 'grupo'),)
+
+    def __str__(self):
+        return f'{self.usuario} - {self.grupo}'
